e64aeee33c3a2c613ba2a31adc51446ce396c160
Akash <88738817+codedmachine111@users.noreply.github.com>
2021-11-21 16:16:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
+#Enter the scrip codes of stocks and start date
+#example:- codes=['500180','500209','532540','500247','507685','500325','500112','532281',]
+#example:- start_date='2021-01-01'
+
 codes=[]
-start_date='2021-01-01'
+start_date='YYYY-MM-DD'
